e4e1c19201ae6bf518e11772ff28e417749bf6bc
Ritika <ritikagusain08@gmail.com>
2026-05-18 19:17:32 +0530

    TLS fix

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,11 +1,18 @@
 import { Redis } from "ioredis";
 import { env } from "./env.ts";
 
+// export const redis = new Redis(env.REDIS_URL, {
+//   retryStrategy: (times) => {
+//     return Math.min(times * 50, 2000); // Reconnect after up to 2 seconds
+//   },
+// });
 export const redis = new Redis(env.REDIS_URL, {
+  tls: env.REDIS_URL.includes('localhost') ? undefined : {},
   retryStrategy: (times) => {
     return Math.min(times * 50, 2000); // Reconnect after up to 2 seconds
   },
 });
+
 
 redis.on("error", (err) => {
   console.error("[Redis Error] Failed to connect:", err.message);
